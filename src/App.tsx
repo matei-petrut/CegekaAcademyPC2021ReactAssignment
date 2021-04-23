a027fa9e8179c71ba6b676945938f21d3e068601
matei-petrut <petrutmatei98@gmail.com>
2021-04-23 11:59:03 +0300

    integrate main section for photos to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <BrowserRouter>
       <Nav />
+      <Main />
     </BrowserRouter>
   );
 }
